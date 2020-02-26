f60ad26e143fd455db14f51efe8288a44278542e
akups <appiah-lindberg@outlook.com>
2020-02-26 16:30:47 +0100

    user schema

--- Kita-Schuessel/backend/models/User.js
+++ Kita-Schuessel/backend/models/User.js
@@ -1,17 +1,22 @@
-const { Schema, model } = require('mongoose');
-const PLM = require('passport-local-mongoose');
+const { Schema, model } = require("mongoose");
+const PLM = require("passport-local-mongoose");
 
-const userSchema = new Schema(
-  {
-    email: String,
-    name: String
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true,
-    versionKey: false
+  email: {
+    type: String,
+    required: true
+  },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: [parent, government, owner]
   }
-);
+});
 
-userSchema.plugin(PLM, { usernameField: 'email' });
+userSchema.plugin(PLM, { usernameField: "email" });
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
